ea3373d33054c33b6fbbe8b5a2b5153ebe29faf5
Dmytro Mishchuk <dmi@adorsys.com.ua>
2020-10-12 14:02:43 +0300

    added Aes128GcmEncryptor, added decryption for login_hint

--- services/src/main/java/org/keycloak/protocol/ciba/resolvers/DefaultCIBALoginUserResolver.java
+++ services/src/main/java/org/keycloak/protocol/ciba/resolvers/DefaultCIBALoginUserResolver.java
@@ -1,12 +1,19 @@
 package org.keycloak.protocol.ciba.resolvers;
 
+import org.apache.commons.lang.StringUtils;
+import org.jboss.logging.Logger;
+import org.keycloak.crypto.Aes128GcmEncryptor;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.protocol.ciba.decoupledauthn.DelegateDecoupledAuthenticationProvider;
+
+import java.security.GeneralSecurityException;
 
 public class DefaultCIBALoginUserResolver implements CIBALoginUserResolver {
 
     private KeycloakSession session;
+    private static final Logger logger = Logger.getLogger(DelegateDecoupledAuthenticationProvider.class);
 
     public DefaultCIBALoginUserResolver(KeycloakSession session) {
         this.session = session;
@@ -14,6 +21,15 @@ public class DefaultCIBALoginUserResolver implements CIBALoginUserResolver {
 
     @Override
     public UserModel getUserFromLoginHint(String loginHint) {
+        String secret = session.getContext().getClient().getSecret();
+        if (StringUtils.isNotBlank(secret)) {
+            try {
+                loginHint = new String(Aes128GcmEncryptor.decrypt(secret, loginHint.getBytes()));
+            } catch (GeneralSecurityException e) {
+                logger.error(e.getMessage());
+                return null;
+            }
+        }
         return KeycloakModelUtils.findUserByNameOrEmail(session, session.getContext().getRealm(), loginHint);
     }
 
